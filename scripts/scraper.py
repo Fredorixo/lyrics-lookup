98d5935de985cdb67f1b496dc3d9931bb4f4583c
Fredorixo <manaskaushik.stpaul@gmail.com>
2023-12-31 12:21:59 +0530

    File Creation

---
+++ scripts/scraper.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from lyricsgenius import Genius
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+load_dotenv()
+
+genius = Genius(os.getenv("GENIUS_ACCESS_TOKEN"), remove_section_headers = True)
+client = MongoClient(os.getenv("CONNECTION_STRING"), server_api = ServerApi('1'))
+db = client.dev
+
+artists = ["Andy Shauf", "Alan Walker", "Marshmello", "Daler Mehndi", "Arijit Singh"]
+
+for artist in artists:
+    try:
+        songs = (genius.search_artist(artist, max_songs = 10, sort = "popularity")).songs
+        for song in songs:
+            db.songs.insert_one({
+                "title": song.title,
+                "artist": song.artist,
+                "lyrics": song.lyrics.partition("Lyrics")[-1],
+                "url": song.url
+            })
+    except:
+        print("Error")
