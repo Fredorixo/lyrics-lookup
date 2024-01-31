1e68010d9d322adc5d4544ba8c8b2e07fb756b14
Fredorixo <manaskaushik.stpaul@gmail.com>
2024-01-31 21:27:44 +0530

    Add Filter to Extended list of Artists

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from os import getenv
 from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 from fastapi import FastAPI, HTTPException, status
-# from sentence_transformers import util
-# from torch import topk
+from sentence_transformers import SentenceTransformer, util
 
 load_dotenv()
 
-client = MongoClient(getenv("CONNECTION_STRING"), server_api = ServerApi('1'))
-db = client.dev
+model = SentenceTransformer('all-MiniLM-L6-v2')
+client = MongoClient(getenv("CONNECTION_STRING"))
+collection = client["dev"]["songs"]
 
 app = FastAPI()
 
 @app.get("/get-songs")
 def get_songs(lyrics: str):
     try:
-        songs = db.songs.find({"$text": {"$search": lyrics}}).limit(10)
-        return songs
+        lyrics_embeddings = [document["embeddings"] for document in collection.find()]
+        query_embedding = model.encode(sentences = lyrics, convert_to_tensor = True)
+
+        results = util.semantic_search(
+            query_embeddings = query_embedding,
+            corpus_embeddings = lyrics_embeddings,
+            top_k = 10
+        )[0]
+
+        return {"Results": [result["corpus_id"] for result in results]}
     except:
         raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR)
--- scraper.py
+++ scraper.py
@@ -2,39 +2,44 @@ from os import getenv
 from dotenv import load_dotenv
 from lyricsgenius import Genius
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 from sentence_transformers import SentenceTransformer
 
 load_dotenv()
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 genius = Genius(access_token = getenv("GENIUS_ACCESS_TOKEN"), remove_section_headers = True)
-client = MongoClient(host = getenv("CONNECTION_STRING"), server_api = ServerApi('1'))
-db = client["dev"]["songs"]
+client = MongoClient(host = getenv("CONNECTION_STRING"))
+collection = client["dev"]["songs"]
 
-artists = [item["item"]["name"] for item in (genius.charts(
-    type_ = "artists",
-    time_period = "all_time",
-    per_page = 50
-))["chart_items"]]
+artists = []
+
+for page in [1, 2]:
+    for result in (genius.charts(type_ = "artists", per_page = 50, page = page))["chart_items"]:
+        artist = result["item"]["name"]
+
+        if artist.isascii() and "Genius" not in artist:
+            artists.append(artist)
 
 for artist in artists:
     try:
         songs = (genius.search_artist(
             artist_name = artist,
-            max_songs = 20,
+            max_songs = 30,
             sort = "popularity"
         )).songs
         
         for song in songs:
+            if(collection.find({"title": song.title, "artist": song.artist})):
+                continue
+
             song_lyrics = " ".join(song.lyrics.partition("Lyrics")[-1].split())
 
-            db.insert_one({
+            collection.insert_one({
+                "_id": collection.estimated_document_count() + 1,
                 "title": song.title,
                 "artist": song.artist,
-                "lyrics": model.encode(sentences = song_lyrics),
-                "url": song.url,
-                "position": db.count_documents + 1
+                "embeddings": model.encode(sentences = song_lyrics, convert_to_tensor = True),
+                "url": song.url
             })
     except:
         print("Error")
