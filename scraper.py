9e3b8cb75aaf566c7284ce851bd3554e40430db1
Fredorixo <manaskaushik.stpaul@gmail.com>
2024-02-10 19:27:15 +0530

    Update scraper to a cron job

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
+import pycron
 from os import getenv
 from string import digits
+from datetime import datetime
 from dotenv import load_dotenv
 from lyricsgenius import Genius
 from pymongo.mongo_client import MongoClient
@@ -16,48 +18,53 @@ genius = Genius(
 client = MongoClient(host = getenv("CONNECTION_STRING"))
 collection = client["dev"]["songs"]
 
-artists = []
+@pycron.cron("0 0 11 * *")
+async def songs_scraper(timestamp: datetime):
+    artists = []
 
-for page in [1, 2]:
-    for result in (genius.charts(type_ = "artists", per_page = 50, page = page))["chart_items"]:
-        artist = result["item"]["name"]
+    for page in [1, 2]:
+        for result in (genius.charts(type_ = "artists", per_page = 50, page = page))["chart_items"]:
+            artist = result["item"]["name"]
 
-        if "Genius" not in artist:
-            artists.append(artist)
+            if "Genius" not in artist:
+                artists.append(artist)
 
-for artist in artists:
-    try:
-        songs = [song.to_dict() for song in (genius.search_artist(
-            artist_name = artist,
-            max_songs = 30,
-            sort = "popularity"
-        )).songs]
-        
-        for song in songs:
-            if(song["language"] != "en" or collection.count_documents({"title": song["title"], "artist": song["artist"]})):
-                continue
+    for artist in artists:
+        try:
+            songs = [song.to_dict() for song in (genius.search_artist(
+                artist_name = artist,
+                max_songs = 30,
+                sort = "popularity"
+            )).songs]
             
-            text = song["lyrics"].split("Lyrics")[1].split("Embed")[0].rstrip(digits)
-            song_lyrics = " ".join(text.split())
-            embedding = model.encode(sentences = song_lyrics, convert_to_tensor = True)
-            media = []
+            for song in songs:
+                if(song["language"] != "en" or collection.count_documents({"title": song["title"], "artist": song["artist"]})):
+                    continue
+                
+                text = song["lyrics"].split("Lyrics")[1].split("Embed")[0].rstrip(digits)
+                song_lyrics = " ".join(text.split())
+                embedding = model.encode(sentences = song_lyrics, convert_to_tensor = True)
+                media = []
 
-            for medium in song["media"]:
-                media.append({
-                    "provider": medium["provider"],
-                    "type": medium["type"],
-                    "url": medium["url"]
+                for medium in song["media"]:
+                    media.append({
+                        "provider": medium["provider"],
+                        "type": medium["type"],
+                        "url": medium["url"]
+                    })
+
+                collection.insert_one({
+                    "_id": collection.estimated_document_count(),
+                    "title": song["title"],
+                    "artist": song["artist"],
+                    "language": "English",
+                    "date": song["release_date"],
+                    "embedding": embedding.tolist(),
+                    "url": song["url"],
+                    "media": media
                 })
+        except Exception as error:
+            print(error)
 
-            collection.insert_one({
-                "_id": collection.estimated_document_count(),
-                "title": song["title"],
-                "artist": song["artist"],
-                "language": "English",
-                "date": song["release_date"],
-                "embedding": embedding.tolist(),
-                "url": song["url"],
-                "media": media
-            })
-    except Exception as error:
-        print(error)
+if __name__ == "__main__":
+    pycron.start()
